ea0d5abb0a60f97f00de57727b97156b163bba94
Kyle Weiner <kyleweiner@ufl.edu>
2023-03-22 13:51:44 -0400

    Encoder and Menu

---
+++ project 6.py
@@ -0,0 +1,31 @@
+def encode(password):
+    encoded_password = ''
+    for digit in password:
+        encoded_digit = str((int(digit) + 3) % 10)
+        encoded_password += encoded_digit
+    return encoded_password
+
+def decode(encoded_password):
+    pass #good luck
+
+if __name__ == '__main__':
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+
+        choice = input("Please enter an option: ")
+
+        if choice == '1':
+            password = input("Please enter your password to encode: ")
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!")
+        elif choice == '2':
+            password = decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {password}.")
+        elif choice == '3':
+            break
+        else:
+            print("Invalid")
